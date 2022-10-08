9c30a2d9e9f4da440b3cd93dc070ffd2d286a867
hajali-amine <amine.hajali@insat.ucar.tn>
2022-10-08 20:24:33 +0100

    test pre-commit

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
+from prometheus_client import make_wsgi_app
 from services.service import factorial as f
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from prometheus_client import make_wsgi_app
+
 from observability.counter import REQUEST_COUNTER
 
 app = Flask(__name__)
 
+
 @app.route("/<int(signed=True):n>")
-def factorial(n):   
+def factorial(n):
     try:
         fac = jsonify(f(n))
         REQUEST_COUNTER.labels(status=200).inc()
@@ -15,6 +17,7 @@ def factorial(n):
     except:
         REQUEST_COUNTER.labels(status=500).inc()
 
-app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-    '/metrics': make_wsgi_app()
-})
+
+app.wsgi_app = DispatcherMiddleware(
+    app.wsgi_app, {"/metrics": make_wsgi_app()}
+)
--- app/observability/counter.py
+++ app/observability/counter.py
@@ -1,2 +1,3 @@
 from prometheus_client import Counter
-REQUEST_COUNTER = Counter('nb_request', 'Request counter', ["status"])
+
+REQUEST_COUNTER = Counter("nb_request", "Request counter", ["status"])
